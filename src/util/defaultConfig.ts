d010ae5ef782895f9685b3a1fedf5d5b36effc1d
ken7253 <ken7253@gmail.com>
2022-05-12 20:32:46 +0900

    設定ファイルを作成

---
+++ src/util/defaultConfig.ts
@@ -0,0 +1,15 @@
+export interface Config {
+  record: {
+    order: 'ascending' | 'descending',
+    extension: boolean,
+    maxLength: number
+  }
+}
+
+export default <Config>{
+  record: {
+    order: 'ascending',
+    extension: true,
+    maxLength: 10,
+  }
+}
